f50abc9197da6215d3a4f642c3cb9d29667dcfb7
r4fa3l <neofto@hotmail.com>
2018-01-26 17:45:48 +0900

    First commit

---
+++ src/App.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+import './App.css';
+
+let defaultTextColor ="#fff";
+let defaultStyle = {
+  color: defaultTextColor
+};
+
+let fakeServerData = {
+  user: {
+    name: 'Rafael',
+    playlists: [
+      {
+        name: 'Rock',
+        songs: [
+         {name: 'Killing in the name of', duration: 1345},
+         {name: 'Master of Puppets', duration: 1234}, 
+         {name: 'Higway to Hell', duration: 1235}
+        ]
+      },
+      {
+        name: 'Programming music',
+        songs: [
+          {name: 'Social Network OST', duration: 1345},
+          {name: 'Rattat', duration: 1234}, 
+          {name: 'Tron', duration: 1235}
+         ]
+      },
+      {
+      name: 'Radiohead',
+      songs: [
+        {name: 'Paranoid Android', duration: 1345},
+        {name: 'Everyone Can Play Guitar', duration: 1234}, 
+        {name: 'Creep', duration: 1235}
+       ]
+    },
+    {
+    name: 'MPB',
+    songs: [
+      {name: 'O Bebado e o equilibrista', duration: 1345},
+      {name: 'Faz Parte do Meu Show', duration: 1234}, 
+      {name: 'Sozinho', duration: 1235}
+     ]
+  }   
+    ]
+  }
+};
+
+class PlaylistCounter extends Component {
+  render() {
+    return (
+      <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
+        <h2>{this.props.playlists.length} playlists</h2>
+      </div>
+    )
+  }
+}
+
+class HoursCounter extends Component {
+  render() {
+    let allSongs = this.props.playlists.reduce((songs, eachPlaylist) => {
+      return songs.concat(eachPlaylist.songs);
+    } , [])
+    let totalDuration = allSongs.reduce((sum, eachSong) => {
+      return sum + eachSong.duration
+    }, 0)
+    return (
+    <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
+    <h2>{Math.round(totalDuration/60)} hours</h2>
+  </div>
+    )
+  }
+}
+
+class Filter extends Component {
+  render() {
+    return(
+      <div style={defaultStyle} >
+        <img />
+        <input type="text" onKeyUp={event => 
+          this.props.onTextChange(event.target.value)} />
+        </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    let playlist = this.props.playlist
+    return(
+      <div style={{...defaultStyle, width: '25%', display: 'inline-block'}} >
+        <img />
+        <h3>{playlist.name}</h3>
+        <ul>
+          {playlist.songs.map(song =>
+          <li>{song.name}</li>
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  constructor () {
+    super();
+    this.state = {
+      serverData: {},
+      filterString: ''
+    }
+  }
+  componentDidMount() {
+    setTimeout(() => {
+    this.setState({serverData: fakeServerData});
+  }, 1000);
+  }
+
+  render() {
+    let playlistToRender = this.state.serverData.user ? this.state.serverData.user.playlists
+    .filter(playlist =>
+      playlist.name.toLowerCase().includes(
+        this.state.filterString.toLowerCase())
+      ) : []
+    return (
+      <div className="App">
+        {this.state.serverData.user ?
+        <div>
+         <h1 style={{...defaultStyle, 'font-size': '54px'}} >
+          {this.state.serverData.user.name}'s Playlist
+         </h1>
+        <PlaylistCounter playlists={playlistToRender} />
+        <HoursCounter playlists={playlistToRender} />
+        <Filter onTextChange={text => {
+          this.setState({filterString: text})
+        }} />
+        {playlistToRender.map(playlist => 
+         <Playlist playlist={playlist} />
+      )}
+
+        </div> : <h1>Loading...</h1>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
